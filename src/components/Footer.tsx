71e2cdab453ae0d214d4e9c73dd67b472f904522
Santiago Pintus <santiagopintus@gmail.com>
2026-06-10 10:49:26 -0300

    Componentized footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="py-8 text-center text-[1.4rem] text-[#212529]">
+      Diseñado y Desarrollado por{' '}
+      <a
+        href="https://www.santiagopintus.dev/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-[#FF9D5C] no-underline hover:underline"
+      >
+        Santiago Pintus
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
