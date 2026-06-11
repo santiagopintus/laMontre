c315fcd2412475727969fa4f530e4f111fc98f32
Santiago Pintus <santiagopintus@gmail.com>
2026-06-11 00:38:50 -0300

    Fixed _document error. Updated package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" className={notoSerif.variable}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=menu&display=optional"
+        />
+      </head>
       <body>
         <CartProvider>
           <Header />
